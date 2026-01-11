0d967d79a31b5d342a05c2ace2b9c91c04b6bf56
annechh <142426482+annechh@users.noreply.github.com>
2026-01-11 17:52:56 +0100

    chore: ongoing homepage developemnt

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,10 @@
 import { useTranslation } from 'react-i18next';
 import HOV_1 from '../assets/images/hov1.webp';
 
-import Vector2 from '../assets/svg/Vector2.svg?react';
+// import Vector2 from '../assets/svg/Vector2.svg?react';
 import Trees3 from '../assets/svg/3-trees.svg?react';
-import TreeGroup3 from '../assets/svg/Trees-group3.svg?react';
-import TreeGroup4 from '../assets/svg/Trees-group4.svg?react';
+// import TreeGroup3 from '../assets/svg/Trees-group3.svg?react';
+// import TreeGroup4 from '../assets/svg/Trees-group4.svg?react';
 
 import FishingIcon from '../assets/svg/fishing.svg?react';
 import HikeIcon from '../assets/svg/hike.svg?react';
@@ -19,6 +19,13 @@ import GoogleMaps from '../components/GoogleMaps';
 export default function Home() {
   const { t } = useTranslation();
   const { cookiesAccepted } = useCookieConsent();
+
+  function renderTextWithParagraphs(text: string): React.ReactNode[] {
+    return text
+      .split('\n')
+      .filter((line) => line.trim())
+      .map((paragraph, index) => <p key={index}>{paragraph}</p>);
+  }
 
   return (
     <>
@@ -48,21 +55,24 @@ export default function Home() {
                 }
               }}
             >
-              Book reisen din
+              {t('common.bookingButton')}
             </button>
           </div>
-          <div className="w-full absolute left-1/2 bottom-0 translate-x-[-50%] translate-y-[50%]">
-            <Vector2 className="w-full block text-secondary" />
-          </div>
+          {/* <div className="w-full absolute left-1/2 bottom-0 translate-x-[-50%] translate-y-[50%]">
+            <Vector2 className="w-full text-secondary" />
+          </div> */}
         </div>
       </header>
 
-      <div className="main-content">
+      <div className="main-content w-full">
         {/* Section 2 */}
+
         <div className="section-container">
           <section className="flex flex-col max-w-7xl gap-12">
-            <h2 className="font-secondary text-5xl font-bold uppercase">{t('test.title')}</h2>
-            <p className="text-3xl">{t('test.text')}</p>
+            <h2 className="font-secondary text-5xl font-bold uppercase">
+              {t('homePage.section_1.title')}
+            </h2>
+            <div className="text-3xl">{renderTextWithParagraphs(t('homePage.section_1.text'))}</div>
 
             <div className="w-full flex justify-center">
               <Trees3 className="text-secondary" />
@@ -72,52 +82,54 @@ export default function Home() {
 
         {/* Section 3 */}
         <div className="section-container bg-light-green">
-          <div className="relative max-w-60 max-h-60 sm:max-w-100 sm:max-h-100 md:max-w-125 md:max-h-125 lg:max-w-150 lg:max-h-150 w-full h-full aspect-square flex  items-center justify-center">
-            <div className="absolute top-0 left-1/2 -translate-x-1/2 w-[40%] aspect-square -mt-7.5 rotate-45 flex items-center justify-center bg-secondary p-2">
-              <div className="w-full h-full overflow-hidden">
-                <img
-                  src={HOV_1}
-                  alt="Top"
-                  className="w-full h-full object-cover scale-150 -rotate-45"
-                />
+          <div className="flex flex-col md:flex-row items-center bg-red-300 gap-20">
+            <div className="relative bg-yellow-200 max-w-48 max-h-48 sm:max-w-100 sm:max-h-100 w-full h-full aspect-square flex items-center justify-center">
+              <div className="absolute sm:top-0 sm:left-1/2 sm:-translate-x-1/2 aspect-square rotate-45 flex items-center justify-center bg-secondary p-1">
+                <div className="w-full h-full overflow-hidden">
+                  <img
+                    src={HOV_1}
+                    alt="Top"
+                    className="w-full h-full object-cover scale-150 -rotate-45"
+                  />
+                </div>
+              </div>
+              {/* <div className="relative max-w-60 max-h-60 sm:max-w-100 sm:max-h-100 md:max-w-125 md:max-h-125 lg:max-w-150 lg:max-h-150 w-full h-full aspect-square flex items-center justify-center">
+              <div className="absolute top-0 left-1/2 -translate-x-1/2 w-[40%] aspect-square -mt-7.5 rotate-45 flex items-center justify-center bg-secondary p-2">
+                <div className="w-full h-full overflow-hidden">
+                  <img
+                    src={HOV_1}
+                    alt="Top"
+                    className="w-full h-full object-cover scale-150 -rotate-45"
+                  />
+                </div>
+              </div> */}
+              <div className="absolute top-1/2 right-0 -translate-y-1/2 w-[40%] aspect-square rotate-45 items-center justify-center bg-secondary p-1 hidden sm:flex">
+                <div className="w-full h-full overflow-hidden">
+                  <img
+                    src={HOV_1}
+                    alt="Top"
+                    className="w-full h-full object-cover scale-150 -rotate-45"
+                  />
+                </div>
               </div>
             </div>
-            <div className="absolute top-1/2 left-0 -translate-y-1/2 w-[40%] aspect-square rotate-45 flex items-center justify-center bg-secondary p-2">
-              <div className="w-full h-full overflow-hidden">
-                <img
-                  src={HOV_1}
-                  alt="Top"
-                  className="w-full h-full object-cover scale-150 -rotate-45"
-                />
-              </div>
-            </div>
-          </div>
 
-          <div className="flex flex-col gap-5 lg:gap-10 lg:justify-center max-w-70 max-h-80 sm:max-w-100 sm:max-h-100 md:max-w-125 md:max-h-125 lg:max-w-150 lg:max-h-150 w-full h-full ">
-            <h2 className="h2">Om oss - Hov Hyttegend</h2>
-            <p className="text">
-              Hov Hyttegrend, ein liten og fredfull hyttegrend midt i hjartet av vakre Fjord-Noreg.
-              Mellom fjell, fossar, fjordar og brear! Her får du eit perfekt utgangspunkt for å
-              oppleve noko av det finaste naturen i Vestland har å by på. .
-            </p>
-            <p className="text">
-              Vi ligg like ved det verna Gaularvassdraget, kjent for sine mange fossar, stryk og
-              stille fjellvatn. Elva svingar seg gjennom landskapet og byr på både store
-              naturopplevingar og gode fiskemoglegheiter.
-            </p>
-            <div>
-              <button className="button-green">Mer om oss</button>
+            {/* <div className="flex flex-col gap-5 lg:gap-10 lg:justify-center max-w-70 max-h-80 sm:max-w-100 sm:max-h-100 md:max-w-125 md:max-h-125 lg:max-w-150 lg:max-h-150 w-full h-full "> */}
+            <div className="flex flex-col gap-5 lg:gap-10 lg:justify-center  w-full h-full ">
+              <h2 className="h2">{t('homePage.section_2.title')}</h2>
+              <p className="text">{t('homePage.section_2.text')}</p>
+
+              <div>
+                <button className="button-green">{t('common.bookingButton')}</button>
+              </div>
             </div>
           </div>
         </div>
 
         {/* section 4 */}
         <section className="section-container">
-          <h2 className="h2">Aktivt friluftsliv – rett utanfor døra</h2>
-          <p>
-            Området rundt hyttene er ideelt for deg som ønskjer å vere aktiv ute i naturen. Her kan
-            du:
-          </p>
+          <h2 className="h2">{t('homePage.section_3.title')}</h2>
+          <p className="text">{t('homePage.section_3.text')}</p>
           <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-5 w-full max-w-7xl">
             <div className="icon-button icon-button-top">
               <MountainHikeIcon className="max-h-12 md:max-h-20 lg:max-h-28 text-primary" />
@@ -140,14 +152,14 @@ export default function Home() {
           </div>
 
           <div className="w-full">
-            <div className="flex justify-between border-b-3 border-secondary">
+            {/* <div className="flex justify-between border-b-3 border-secondary">
               <TreeGroup3 className="text-secondary" />
               <TreeGroup4 className="text-secondary" />
             </div>
             <div className="flex justify-between rotate-x-180">
               <TreeGroup3 className="text-secondary" />
               <TreeGroup4 className="text-secondary" />
-            </div>
+            </div> */}
           </div>
 
           <div className="grid grid-cols-1 sm:grid-cols-3 gap-5 w-full max-w-7xl">
