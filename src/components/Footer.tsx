7522d3897db554048f9677a4b86619b9e17b0cee
annechh <142426482+annechh@users.noreply.github.com>
2026-01-12 17:50:09 +0100

    Chore: footer under development

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,29 +6,41 @@ export default function Footer() {
       <div className="flex flex-col lg:flex-row justify-center">
         <HovLogo className="h-24" />
         <div className="flex flex-col gap-5">
-          <ul>
-            <li>Terms and conditions</li>
-            <li>Privacy policy</li>
-          </ul>
+          <div>
+            <h3 className="">Lenker</h3>
+            <ul>
+              <li>Terms and conditions</li>
+              <li>Privacy policy</li>
+            </ul>
+          </div>
 
-          <ul>
-            <li>post@hovhyttegrend.no</li>
-            <li>+47 400 67 568</li>
-            <li className="flex flex-col ">
-              Åpningstider:<span>09:00 - 11:00 | 18:30 - 20:00</span>
-            </li>
-          </ul>
+          <div>
+            <h3 className="">Kontakt</h3>
+            <ul>
+              <li>post@hovhyttegrend.no</li>
+              <li>+47 400 67 568</li>
+              <li className="flex flex-col ">
+                Åpningstider:<span>09:00 - 11:00 | 18:30 - 20:00</span>
+              </li>
+            </ul>
+          </div>
 
-          <ul>
-            <li>Hov Hyttegrend</li>
-            <li>Gaularfjellsvegen 2203</li>
-            <li>6978 Viksdalen</li>
-          </ul>
+          <div>
+            <h3 className="">Finn oss</h3>
+            <ul>
+              <li>Hov Hyttegrend</li>
+              <li>Gaularfjellsvegen 2203</li>
+              <li>6978 Viksdalen</li>
+            </ul>
+          </div>
 
-          <ul>
-            <li>Instagram</li>
-            <li>Facebook</li>
-          </ul>
+          <div>
+            <h3 className="">Følg oss</h3>
+            <ul>
+              <li>Instagram</li>
+              <li>Facebook</li>
+            </ul>
+          </div>
         </div>
       </div>
 
