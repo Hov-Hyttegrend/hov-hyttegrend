044953d8d373681d4e329516816c73132a74ff0c
annechh <142426482+annechh@users.noreply.github.com>
2026-01-14 18:14:51 +0100

    Chore: adds background image as component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,18 @@
 import { Outlet } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
+import Background from './Background';
+import HOV_7 from '../assets/images/hov7.webp';
 
 export default function Layout() {
   return (
     <div className="min-h-screen flex flex-col w-full font-primary">
+      <Background
+        imageUrl={HOV_7}
+        imageAlt="Waterfall down to a lake surrounded by mountain and trees"
+      />
       <Header />
-      <main className="bg-primary grow flex flex-col  items-center ">
+      <main className="grow flex flex-col  items-center ">
         <Outlet />
       </main>
       <Footer />
