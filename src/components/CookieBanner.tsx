59f4e2bd2a409e54d2ea5986da6afe61d6ff6d40
annechh <142426482+annechh@users.noreply.github.com>
2026-01-18 17:15:01 +0100

    Chore: adds translation to cookie banner

--- src/components/CookieBanner.tsx
+++ src/components/CookieBanner.tsx
@@ -1,9 +1,11 @@
+import { useTranslation } from 'react-i18next';
 import { useCookieConsent } from '../contexts/useCookieConsent';
 import { useEffect, useState } from 'react';
 
 export default function CookieBanner() {
   const { acceptCookies, declineCookies } = useCookieConsent();
   const [showBanner, setShowBanner] = useState(false);
+  const { t } = useTranslation();
 
   useEffect(() => {
     const consent = localStorage.getItem('cookieConsent');
@@ -23,28 +25,25 @@ export default function CookieBanner() {
   return (
     <div className="fixed bottom-0 left-0 right-0 bg-gray-900 text-white p-4 shadow-lg z-50">
       <div className="max-w-7xl mx-auto flex flex-col sm:flex-row items-center justify-between gap-4">
-        <p className="text-sm">
-          This website uses cookies for booking functionality and maps. By continuing, you need to
-          accept our use of cookies.
-        </p>
+        <p className="text-sm">{t('common.cookieConcent.description')}</p>
         <div className="flex gap-3">
           <button
             onClick={() => {
               declineCookies();
               setShowBanner(false);
             }}
-            className="px-4 py-2 bg-gray-700 hover:bg-gray-600 rounded text-sm transition"
+            className="px-4 py-2 bg-gray-700 hover:bg-gray-600 rounded-full text-sm transition cursor-pointer"
           >
-            Decline
+            {t('common.cookieConcent.buttonDecline')}
           </button>
           <button
             onClick={() => {
               acceptCookies();
               setShowBanner(false);
             }}
-            className="px-4 py-2 bg-yellow-500 hover:bg-yellow-600 rounded text-sm font-semibold transition"
+            className="px-4 py-2 bg-yellow-500 hover:bg-yellow-600 rounded-full text-sm font-semibold transition text-black hover:text-white cursor-pointer"
           >
-            Accept
+            {t('common.cookieConcent.buttonAccept')}
           </button>
         </div>
       </div>
