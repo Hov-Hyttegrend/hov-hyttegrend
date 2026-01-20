8118d3e7dc368fe149e9528e05446d211d2224de
annechh <142426482+annechh@users.noreply.github.com>
2026-01-20 12:45:55 +0100

    style: updated footer with styles and moved decorative trees to top of footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,65 +1,74 @@
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 import HovLogo from '../assets/svg/Hov-logo.svg?react';
+import GroupTrees3 from '../assets/svg/GroupTrees3.svg?react';
+import GroupTrees4 from '../assets/svg/GroupTrees4.svg?react';
 
 export default function Footer() {
   const { t } = useTranslation();
 
   return (
-    <footer className="relative bg-secondary text-white w-full py-20 px-7">
-      <div className="flex flex-col lg:flex-row justify-center">
-        <HovLogo className="h-24" />
-        <div className="flex flex-col gap-5">
-          <div>
-            <h3 className="font-bold uppercase">{t('common.links.title')}</h3>
-            <ul>
-              <li>
-                <Link to="/terms_and_conditions" className="hover:underline">
-                  {t('common.links.terms')}
-                </Link>
-              </li>
-              <li>
-                <Link to="/privacy_policy" className="hover:underline">
-                  {t('common.links.privacy')}
-                </Link>
-              </li>
-            </ul>
-          </div>
-
-          <div>
-            <h3 className="font-bold uppercase">{t('common.contact.title')}</h3>
-            <ul>
-              <li>post@hovhyttegrend.no</li>
-              <li>+47 400 67 568</li>
-              <li className="flex flex-col ">
-                {t('common.contact.openingHoursTitle')}
-                <span>09:00 - 11:00 | 18:30 - 20:00</span>
-              </li>
-            </ul>
-          </div>
-
-          <div>
-            <h3 className="font-bold uppercase">{t('common.findUs.title')}</h3>
-            <ul>
-              <li>Hov Hyttegrend</li>
-              <li>Gaularfjellsvegen 2203</li>
-              <li>6978 Viksdalen</li>
-            </ul>
-          </div>
-
-          <div>
-            <h3 className="font-bold uppercase">{t('common.followUs.title')}</h3>
-            <ul>
-              <li>Instagram</li>
-              <li>Facebook</li>
-            </ul>
-          </div>
-        </div>
+    <footer className="text-white w-full flex flex-col">
+      <div className="w-full bg-primary flex justify-between items-baseline max-h-">
+        <GroupTrees3 className="text-secondary max-h-14 md:max-h-28 xl:max-h-56" />
+        <GroupTrees4 className="text-secondary max-h-12 md:max-h-24 xl:max-h-48" />
       </div>
 
-      <p className="w-full text-center text-xs">
-        &copy; {new Date().getFullYear()} Hov Hyttegrend. All rights reserved.
-      </p>
+      <div className="flex flex-col justify-center items-start md:items-center w-full bg-secondary pt-20 pb-10 px-7">
+        <HovLogo className="h-16 md:h-24 mb-5" />
+        <div className="flex justify-center items-center mt-10">
+          <div className="flex flex-wrap justify-start gap-8 lg:gap-16">
+            <div className="footer-ul">
+              <h3 className="md:text-lg font-bold uppercase mb-2">{t('common.links.title')}</h3>
+              <ul>
+                <li className="footer-list hover:underline cursor-pointer">
+                  <Link to="/terms_and_conditions" className="hover:underline">
+                    {t('common.links.terms')}
+                  </Link>
+                </li>
+                <li className="footer-list hover:underline cursor-pointer">
+                  <Link to="/privacy_policy" className="hover:underline">
+                    {t('common.links.privacy')}
+                  </Link>
+                </li>
+              </ul>
+            </div>
+
+            <div className="footer-ul">
+              <h3 className="md:text-lg font-bold uppercase mb-2">{t('common.findUs.title')}</h3>
+              <ul className="">
+                <li className="footer-list">Hov Hyttegrend</li>
+                <li className="footer-list">Gaularfjellsvegen 2203</li>
+                <li className="footer-list">6978 Viksdalen</li>
+              </ul>
+            </div>
+
+            <div className="footer-ul">
+              <h3 className="md:text-lg font-bold uppercase mb-2">{t('common.contact.title')}</h3>
+              <ul>
+                <li className="footer-list">post@hovhyttegrend.no</li>
+                <li className="footer-list">+47 400 67 568</li>
+                <li className="footer-list flex flex-col">
+                  {t('common.contact.openingHoursTitle')}
+                  <span className="text-xs">09:00 - 11:00 | 18:30 - 20:00</span>
+                </li>
+              </ul>
+            </div>
+
+            <div className="footer-ul">
+              <h3 className="md:text-lg font-bold uppercase mb-2">{t('common.followUs.title')}</h3>
+              <ul>
+                <li className="footer-list hover:underline cursor-pointer">Instagram</li>
+                <li className="footer-list hover:underline cursor-pointer">Facebook</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <p className="w-full md:text-center text-xs text-gray-300 mt-10">
+          &copy; {new Date().getFullYear()} Hov Hyttegrend. All rights reserved.
+        </p>
+      </div>
     </footer>
   );
 }
