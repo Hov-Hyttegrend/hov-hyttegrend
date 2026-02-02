5f72a76368e1a323a633a5f9c3da1824e6564d7e
annechh <142426482+annechh@users.noreply.github.com>
2026-02-02 18:40:40 +0100

    deleted: unused files deleted

--- src/utils/renderTextWithParagraphs.tsx
+++
@@ -1,6 +0,0 @@
-export function renderTextWithParagraphs(text: string): React.ReactNode[] {
-  return text
-    .split('\n')
-    .filter((line) => line.trim())
-    .map((paragraph, index) => <p key={index}>{paragraph}</p>);
-}
