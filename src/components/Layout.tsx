6ba313b4c0aa37de69ce302d786d8e96197c3d31
annechh <142426482+annechh@users.noreply.github.com>
2026-02-20 09:39:50 +0100

    Feat: adds useScrollToHash for navigation

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,8 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
+import { useScrollToHash } from '../hooks/useScrollToHash';
 
 export default function Layout() {
+  useScrollToHash();
+
   return (
     <div className="min-h-screen flex flex-col w-full font-primary">
       <Header />
